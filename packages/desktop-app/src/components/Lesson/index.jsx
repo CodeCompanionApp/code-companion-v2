4c0171176d9ae36262ea0e51469b97d6c02d3d09
Hamish Macpherson <hamstu@gmail.com>
2017-09-06 20:29:27 -0700

    add button for opening devtools

--- packages/desktop-app/src/components/Lesson/index.jsx
+++ packages/desktop-app/src/components/Lesson/index.jsx
@@ -4,6 +4,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+/**
+ * Return the src for the lesson to show in webview
+ * @param  {object} lesson lesson manifest
+ * @return {string}
+ */
 const getLessonSrc = lesson => url.format({
   pathname: path.join(lesson.path, lesson.main),
   protocol: 'file:',
@@ -11,15 +16,24 @@ const getLessonSrc = lesson => url.format({
 });
 
 class Lesson extends Component {
-  componentDidMount() {
-    this.wv.addEventListener('dom-ready', () => {
-      this.wv.openDevTools();
-    });
+  constructor() {
+    super();
+    this.openLessonDevTools = this.openLessonDevTools.bind(this);
+  }
+  componentDidUpdate() {
+    if (this.webview) {
+      this.webview.addEventListener('dom-ready', () => {
+        // this.openLessonDevTools();
+      });
+    }
+  }
+  openLessonDevTools() {
+    this.webview.openDevTools();
   }
   renderLessonWebView() {
     return this.props.lesson ?
       <webview
-        ref={(wv) => { this.wv = wv; }}
+        ref={(webview) => { this.webview = webview; }}
         id="foo"
         src={getLessonSrc(this.props.lesson)}
         style={{ display: 'inline-flex', width: '780px', height: '470px', border: '1px solid #ccc' }}
@@ -29,7 +43,12 @@ class Lesson extends Component {
   render() {
     return (
       <div>
-        <p>This is a lesson!</p>
+        <p>
+          This is a lesson!
+          <button onClick={this.openLessonDevTools}>
+            Open DevTools for Lesson
+          </button>
+        </p>
         {this.renderLessonWebView()}
       </div>
     );
